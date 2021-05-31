d97251c933071c54e8f834d2efddbfae2661c5c9
Eugenio Panadero <eugenio.panadero@gmail.com>
2021-05-31 21:54:38 +0200

    parser: adapt to new tariff format, using PCB/CYM price identifiers

--- aiopvpc/pvpc_download.py
+++ aiopvpc/pvpc_download.py
@@ -4,6 +4,7 @@ Simple aio library to download Spanish electricity hourly prices.
 * URL for JSON daily files
 * Parser for the contents of the JSON files
 """
+import logging
 import sys
 from datetime import date, datetime, timedelta
 from typing import Any, Dict, Optional, Union
@@ -15,16 +16,20 @@ else:  # pragma: no cover
 
 # Tariffs as internal keys in esios API data
 ESIOS_TARIFFS = ["GEN", "NOC", "VHC"]
+ESIOS_TARIFFS_NEW = ["PCB", "CYM"]
 
 # Tariffs used in HomeAssistant integration
 TARIFFS = ["normal", "discrimination", "electric_car"]
 TARIFF_KEYS = dict(zip(TARIFFS, ESIOS_TARIFFS))
+TARIFFS_NEW = ["2.0TD", "2.0TD (Ceuta/Melilla)"]
+TARIFF_KEYS_NEW = dict(zip(TARIFFS_NEW, ESIOS_TARIFFS_NEW))
 
 # Prices are given in 0 to 24h sets, adjusted to the main timezone in Spain
 REFERENCE_TZ = zoneinfo.ZoneInfo("Europe/Madrid")
 UTC_TZ = zoneinfo.ZoneInfo("UTC")
 
 DEFAULT_TIMEOUT = 5
+DATE_CHANGE_TO_20TD = date(2021, 6, 1)
 _PRECISION = 5
 _RESOURCE = (
     "https://api.esios.ree.es/archives/70/download_json"
@@ -41,12 +46,21 @@ def extract_pvpc_data(
     data: Dict[str, Any],
     key: Optional[str] = None,
     tz: zoneinfo.ZoneInfo = REFERENCE_TZ,
+    zone_ceuta_melilla: bool = False,
 ) -> Union[Dict[datetime, float], Dict[datetime, Dict[str, float]]]:
     """Parse the contents of a daily PVPC json file."""
     ts_init = datetime(
         *datetime.strptime(data["PVPC"][0]["Dia"], "%d/%m/%Y").timetuple()[:3],
         tzinfo=tz,
     ).astimezone(UTC_TZ)
+
+    if ts_init.date() >= date(2021, 6, 1) and key not in ESIOS_TARIFFS_NEW:
+        logging.warning(
+            "Bad call for prices with new 2.0TD tariff, "
+            "'%s' is not valid anymore :(",
+            key,
+        )
+        key = "CYM" if zone_ceuta_melilla else "PCB"
 
     def _parse_tariff_val(value, prec=_PRECISION) -> float:
         return round(float(value.replace(",", ".")) / 1000.0, prec)
