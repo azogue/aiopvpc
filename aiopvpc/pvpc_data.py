2e06d3522b457cc0980e8bd4ec3075e82d3d8b9f
Iván Povedano <ivan.ipg@gmail.com>
2021-06-02 17:54:24 +0200

    lint corrections

--- aiopvpc/pvpc_data.py
+++ aiopvpc/pvpc_data.py
@@ -175,29 +175,27 @@ class PVPCData:
         actual_time = _local(utc_time)
         # todo current_period, next_period [P1/P2/P3], next_period_in (hours)
         # get period for current time and add sensor attribute
-        def _isFestive():
+        def _is_festive():
             festives = ["01-01", "06-01", "01-05", "12-10", "01-11", "06-12", "08-12", "25-12"]
 
-            ss = ["01-04-2018", "21-04-2019", "12-04-2020", "04-04-2021", "17-04-2022", "09-04-2023", "31-03-2024", "20-04-2025", "05-04-2026", "28-03-2027",
-                "16-04-2028", "01-04-2029", "21-04-2030", "13-04-2031", "28-03-2032", "17-04-2033", "09-04-2034", "25-03-2035", "13-04-2036", "05-04-2037",
-                "25-04-2038", "10-04-2039", "01-04-2040", "21-04-2041", "06-04-2042", "29-03-2043", "17-04-2044", "09-04-2045", "25-03-2046", "14-04-2047",
-                "05-04-2048", "18-04-2049", "10-04-2050", "02-04-2051", "21-04-2052", "06-04-2053", "29-03-2054", "18-04-2055", "02-04-2056", "22-04-2057"]
+            ss = ["01-04-2018", "21-04-2019", "12-04-2020", "04-04-2021", "17-04-2022", "09-04-2023",
+                "31-03-2024", "20-04-2025", "05-04-2026", "28-03-2027", "16-04-2028", "01-04-2029",
+                "21-04-2030", "13-04-2031", "28-03-2032", "17-04-2033", "09-04-2034", "25-03-2035",
+                "13-04-2036", "05-04-2037", "25-04-2038", "10-04-2039", "01-04-2040", "21-04-2041",
+                "06-04-2042", "29-03-2043", "17-04-2044", "09-04-2045", "25-03-2046", "14-04-2047",
+                "05-04-2048", "18-04-2049", "10-04-2050", "02-04-2051", "21-04-2052", "06-04-2053",
+                "29-03-2054", "18-04-2055", "02-04-2056", "22-04-2057"]
 
-            formatDate = "%d-%m"
-            formatFullDate = "%d-%m-%Y"
+            format_date = "%d-%m"
+            format_full_date = "%d-%m-%Y"
 
-            if actual_time.strftime(formatDate) in festives:
-                return True
-            elif actual_time.strftime(formatFullDate) in ss:
-                return True
-            else:
-                return False
+            return actual_time.strftime(format_date) in festives or actual_time.strftime(format_full_date) in ss
 
-        dayOfWeek = actual_time.weekday()
+        day_of_week = actual_time.weekday()
         period = "error"
         if actual_time.hour < 8:
             period = "valley"
-        elif dayOfWeek == 6 or dayOfWeek == 0 or _isFestive():
+        elif day_of_week == 6 or day_of_week == 0 or _is_festive():
             period = "valley"
         elif (actual_time.hour >= 10 and actual_time.hour < 14) or (actual_time.hour >= 18 and actual_time.hour < 22):
             period = "peak"
