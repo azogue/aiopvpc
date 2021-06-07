7f0e53b3b0e26a779f58be3ddc8ac76c32f8dd08
Eugenio Panadero <eugenio.panadero@gmail.com>
2021-06-07 21:23:12 +0200

    fix: partial fix for national holidays, and new unit test

---
+++ tests/test_tariff_attributes.py
@@ -0,0 +1,31 @@
+"""Tests for aiopvpc."""
+from datetime import datetime, timedelta
+
+import pytest
+
+from aiopvpc.const import REFERENCE_TZ
+from aiopvpc.pvpc_data import _tariff_period_key
+
+
+@pytest.mark.parametrize(
+    "year, days_weekend_p3, extra_days_p3",
+    (
+        (2021, 104, 7),
+        (2022, 105, 7),
+        (2023, 105, 9),
+    ),
+)
+def test_number_of_national_holidays(year, days_weekend_p3, extra_days_p3):
+    """Calculate days with full P3 valley period."""
+    holidays_p3 = weekend_days_p3 = 0
+    day = datetime(year, 1, 1, 15, tzinfo=REFERENCE_TZ)
+    while day.year == year:
+        if _tariff_period_key(day, False) == "P3":
+            if day.isoweekday() > 5:
+                weekend_days_p3 += 1
+            else:
+                holidays_p3 += 1
+        day += timedelta(days=1)
+    assert weekend_days_p3 == days_weekend_p3
+    # TODO bump python-holidays version to fix Good Friday as national holiday
+    assert holidays_p3 + 1 == extra_days_p3
