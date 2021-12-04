b9e48c66fdf9c5833a714edd9dc1483c1eec8a9f
Eugenio Panadero <eugenio.panadero@gmail.com>
2021-12-04 18:48:26 +0100

    :fire: Remove tests for range-download methods

--- tests/test_pvpc.py
+++ tests/test_pvpc.py
@@ -2,12 +2,11 @@
 import logging
 from asyncio import TimeoutError
 from datetime import datetime, timedelta
-from unittest.mock import patch
 
 import pytest
 from aiohttp import ClientError
 
-from aiopvpc.const import OLD_TARIFS_IDS, REFERENCE_TZ, UTC_TZ
+from aiopvpc.const import REFERENCE_TZ, UTC_TZ
 from aiopvpc.pvpc_data import PVPCData
 from tests.conftest import MockAsyncSession, TZ_TEST
 
@@ -104,79 +103,6 @@ async def test_bad_downloads(
         assert len(caplog.messages) == num_log_msgs
     assert mock_session.call_count == 1
     assert len(prices) == 0
-
-
-@pytest.mark.parametrize(
-    "timezone, start, end",
-    (
-        (
-            TZ_TEST,
-            datetime(2019, 10, 26, 15, tzinfo=TZ_TEST),
-            datetime(2019, 10, 27, 13, tzinfo=TZ_TEST),
-        ),
-        (
-            REFERENCE_TZ,
-            datetime(2019, 10, 26, 15, tzinfo=REFERENCE_TZ),
-            datetime(2019, 10, 27, 13, tzinfo=REFERENCE_TZ),
-        ),
-    ),
-)
-def test_full_data_download_range(timezone, start, end):
-    """Test retrieval of full PVPC data in a day range."""
-    with patch("aiohttp.ClientSession", MockAsyncSession):
-        pvpc_data = PVPCData(local_timezone=timezone)
-        prices = pvpc_data.download_prices_for_range(start, end)
-
-    assert len(prices) == 24
-    first_price = min(prices)
-    last_price = max(prices)
-    data_first_hour = prices[first_price]
-
-    # Check full PVPC data is retrieved
-    assert len(data_first_hour) == 30
-    assert all(tag in data_first_hour for tag in OLD_TARIFS_IDS)
-
-    # Check units have not changed in full data retrieval (they are in €/MWh)
-    assert all(data_first_hour[tag] > 1 for tag in OLD_TARIFS_IDS)
-
-    # check tz-alignment (price at 15h is tz-independent)
-    assert prices[first_price]["NOC"] == 119.16
-    assert first_price.astimezone(timezone).hour == 15
-    assert last_price.astimezone(timezone).hour == 13
-
-
-@pytest.mark.asyncio
-async def test_download_range(caplog):
-    """Test retrieval of full PVPC data in a day range."""
-    start = datetime(2019, 10, 26, 15)
-    end = datetime(2019, 10, 28, 13)
-    mock_session = MockAsyncSession()
-
-    with caplog.at_level(logging.WARNING):
-        pvpc_data = PVPCData(
-            tariff="electric_car", local_timezone=TZ_TEST, websession=mock_session
-        )
-        prices = await pvpc_data.async_download_prices_for_range(start, end)
-        assert mock_session.call_count == 3
-        assert len(prices) == 34
-        assert len(caplog.messages) == 2
-
-        no_prices = await pvpc_data.async_download_prices_for_range(
-            datetime(2010, 8, 27, tzinfo=TZ_TEST),
-            datetime(2010, 8, 27, 22, tzinfo=TZ_TEST),
-        )
-        assert len(no_prices) == 0
-        assert len(caplog.messages) == 4
-        assert not await pvpc_data.async_download_prices_for_range(
-            datetime(2010, 8, 27), datetime(2010, 8, 27, 23)
-        )
-        assert len(caplog.messages) == 7
-
-    first_price = min(prices)
-    assert first_price.hour == 14 and first_price.tzname() == "UTC"
-    # Check only tariff values are retrieved
-    assert isinstance(prices[first_price], float)
-    assert prices[first_price] < 1
 
 
 async def _run_h_step(
