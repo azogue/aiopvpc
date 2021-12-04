bf98d11f8a65ea66a5116443a7d34617af7f5114
Eugenio Panadero <eugenio.panadero@gmail.com>
2021-12-04 18:48:26 +0100

    :fire: Remove tests for range-download methods

--- tests/test_real_api_calls.py
+++ tests/test_real_api_calls.py
@@ -8,38 +8,6 @@ from aiopvpc import PVPCData
 
 
 @pytest.mark.real_api_call
-def test_real_download_range():
-    # No async
-    pvpc_handler = PVPCData("normal")
-    start = datetime(2019, 10, 26, 15)
-    end = datetime(2019, 10, 28, 13)
-    prices = pvpc_handler.download_prices_for_range(start, end)
-    assert len(prices) == 48
-
-    no_prices = pvpc_handler.download_prices_for_range(
-        datetime(2010, 8, 26, 23), datetime(2010, 8, 27, 22)
-    )
-    assert len(no_prices) == 0
-
-
-@pytest.mark.real_api_call
-@pytest.mark.asyncio
-async def test_real_download_range_async():
-    start = datetime(2019, 10, 26, 15)
-    end = datetime(2019, 10, 28, 13)
-    async with ClientSession() as session:
-        pvpc_handler = PVPCData("normal", websession=session)
-        prices = await pvpc_handler.async_download_prices_for_range(start, end)
-    assert len(prices) == 48
-
-    # without session also works, creating one for each download range call
-    pvpc_handler_no_s = PVPCData("normal")
-    prices2 = await pvpc_handler_no_s.async_download_prices_for_range(start, end)
-    assert len(prices2) == 48
-    assert prices == prices2
-
-
-@pytest.mark.real_api_call
 @pytest.mark.asyncio
 async def test_real_download_today_async():
     async with ClientSession() as session:
