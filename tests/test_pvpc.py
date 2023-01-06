b126a2290f5245938ac83719a9d739a83b393e4e
Eugenio Panadero Maciá <eugenio.panadero@gmail.com>
2023-01-06 17:48:58 +0100

    ♻️ tests: Minor refactor

--- tests/test_pvpc.py
+++ tests/test_pvpc.py
@@ -1,6 +1,4 @@
 """Tests for aiopvpc."""
-from __future__ import annotations
-
 import logging
 from asyncio import TimeoutError
 from datetime import datetime, timedelta
@@ -13,15 +11,14 @@ from aiopvpc.const import (
     ALL_SENSORS,
     ATTRIBUTIONS,
     DataSource,
-    EsiosApiData,
     KEY_INYECTION,
     KEY_OMIE,
     KEY_PVPC,
     REFERENCE_TZ,
     UTC_TZ,
 )
-from aiopvpc.pvpc_data import PVPCData, BadApiTokenAuth
-from tests.conftest import MockAsyncSession, TZ_TEST
+from aiopvpc.pvpc_data import BadApiTokenAuthError, PVPCData
+from tests.conftest import check_num_datapoints, MockAsyncSession, run_h_step, TZ_TEST
 
 
 @pytest.mark.parametrize(
@@ -54,7 +51,7 @@ async def test_bad_downloads(
             api_token=api_token,
         )
         if status == 401:
-            with pytest.raises(BadApiTokenAuth):
+            with pytest.raises(BadApiTokenAuthError):
                 await pvpc_data.async_update_all(None, day)
             assert mock_session.call_count == 1
             return
@@ -64,37 +61,7 @@ async def test_bad_downloads(
         assert not pvpc_data.process_state_and_attributes(api_data, KEY_PVPC, day)
         assert len(caplog.messages) == num_log_msgs
     assert mock_session.call_count == 1
-    _check_num_datapoints(api_data, (KEY_PVPC,), 0)
-
-
-async def _run_h_step(
-    mock_session: MockAsyncSession,
-    pvpc_data: PVPCData,
-    api_data: EsiosApiData | None,
-    start: datetime,
-) -> tuple[datetime, EsiosApiData]:
-    current_prices = api_data["sensors"][KEY_PVPC] if api_data else {}
-    if current_prices:
-        logging.debug(
-            "[Calls=%d]-> start=%s --> %s -> %s (%d prices)",
-            mock_session.call_count,
-            start,
-            list(current_prices)[0].strftime("%Y-%m-%d %Hh"),
-            list(current_prices)[-1].strftime("%Y-%m-%d %Hh"),
-            len(current_prices),
-        )
-    api_data = await pvpc_data.async_update_all(api_data, start)
-    assert pvpc_data.process_state_and_attributes(api_data, KEY_PVPC, start)
-    start += timedelta(hours=1)
-    return start, api_data
-
-
-def _check_num_datapoints(
-    api_data: EsiosApiData, sensor_keys: tuple[str, ...], expected: int
-):
-    for key in sensor_keys:
-        num_points = len(api_data["sensors"][key])
-        assert num_points == expected, (key, expected, num_points)
+    check_num_datapoints(api_data, (KEY_PVPC,), 0)
 
 
 # TODO review download schedule for Canary Islands TZ
@@ -125,37 +92,37 @@ async def test_reduced_api_download_rate_dst_change(local_tz, data_source, senso
     # avoid extra calls at day if already got all today prices
     api_data = None
     for _ in range(3):
-        start, api_data = await _run_h_step(mock_session, pvpc_data, api_data, start)
+        start, api_data = await run_h_step(mock_session, pvpc_data, api_data, start)
         assert mock_session.call_count == len(sensor_keys)
-        _check_num_datapoints(api_data, sensor_keys, 24)
+        check_num_datapoints(api_data, sensor_keys, 24)
 
     # first call for next-day prices
     assert start == datetime(2021, 10, 30, 18, tzinfo=UTC_TZ)
-    start, api_data = await _run_h_step(mock_session, pvpc_data, api_data, start)
+    start, api_data = await run_h_step(mock_session, pvpc_data, api_data, start)
     assert mock_session.call_count == 2 * len(sensor_keys)
-    _check_num_datapoints(api_data, sensor_keys, 49)
+    check_num_datapoints(api_data, sensor_keys, 49)
 
     # avoid calls at evening if already got all today+tomorrow prices
     for _ in range(3):
-        start, api_data = await _run_h_step(mock_session, pvpc_data, api_data, start)
+        start, api_data = await run_h_step(mock_session, pvpc_data, api_data, start)
         assert mock_session.call_count == 2 * len(sensor_keys)
-        _check_num_datapoints(api_data, sensor_keys, 49)
+        check_num_datapoints(api_data, sensor_keys, 49)
 
     # avoid calls at day if already got all today prices
     for _ in range(21):
-        start, api_data = await _run_h_step(mock_session, pvpc_data, api_data, start)
+        start, api_data = await run_h_step(mock_session, pvpc_data, api_data, start)
         assert mock_session.call_count == 2 * len(sensor_keys)
         assert api_data["available"]
-        # _check_num_datapoints(api_data, sensor_keys, 25)
+        # check_num_datapoints(api_data, sensor_keys, 25)
 
     # call for next-day prices (no more available)
     assert start == datetime(2021, 10, 31, 19, tzinfo=UTC_TZ)
     call_count = mock_session.call_count
     while start.astimezone(local_tz) <= datetime(2021, 10, 31, 23, tzinfo=local_tz):
-        start, api_data = await _run_h_step(mock_session, pvpc_data, api_data, start)
+        start, api_data = await run_h_step(mock_session, pvpc_data, api_data, start)
         call_count += len(sensor_keys)
         assert mock_session.call_count == call_count
-        # _check_num_datapoints(api_data, sensor_keys, 25)
+        # check_num_datapoints(api_data, sensor_keys, 25)
 
     # assert mock_session.call_count == 6
     assert pvpc_data.states.get(KEY_PVPC)
@@ -165,7 +132,7 @@ async def test_reduced_api_download_rate_dst_change(local_tz, data_source, senso
 
     # After known prices are exausted, the state is flagged as unavailable
     with pytest.raises(AssertionError):
-        start, api_data = await _run_h_step(mock_session, pvpc_data, api_data, start)
+        start, api_data = await run_h_step(mock_session, pvpc_data, api_data, start)
     assert not api_data["available"]
     start += timedelta(hours=1)
     assert not pvpc_data.process_state_and_attributes(api_data, KEY_PVPC, start)
@@ -196,19 +163,19 @@ async def test_reduced_api_download_rate(local_tz, data_source, sensor_keys):
     # avoid extra calls at day if already got all today prices
     api_data = None
     for _ in range(17):
-        start, api_data = await _run_h_step(mock_session, pvpc_data, api_data, start)
+        start, api_data = await run_h_step(mock_session, pvpc_data, api_data, start)
         assert mock_session.call_count == len(sensor_keys)
-        _check_num_datapoints(api_data, sensor_keys, 24)
+        check_num_datapoints(api_data, sensor_keys, 24)
 
     # first call for next-day prices
     assert start == datetime(2023, 1, 6, 19, tzinfo=UTC_TZ)
-    start, api_data = await _run_h_step(mock_session, pvpc_data, api_data, start)
+    start, api_data = await run_h_step(mock_session, pvpc_data, api_data, start)
     assert mock_session.call_count == 2 * len(sensor_keys)
-    _check_num_datapoints(api_data, sensor_keys, 24)
+    check_num_datapoints(api_data, sensor_keys, 24)
 
     call_count = mock_session.call_count
     while start.astimezone(local_tz) <= datetime(2023, 1, 6, 23, tzinfo=local_tz):
-        start, api_data = await _run_h_step(mock_session, pvpc_data, api_data, start)
+        start, api_data = await run_h_step(mock_session, pvpc_data, api_data, start)
         call_count += len(sensor_keys)
         assert mock_session.call_count == call_count
-        _check_num_datapoints(api_data, sensor_keys, 24)
+        check_num_datapoints(api_data, sensor_keys, 24)
